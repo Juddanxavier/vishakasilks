8cd9cc4f6dbfad924a6b597a70dd146c199f4e67
Juddan <juddan2008@gmail.com>
2023-06-20 23:38:48 +0530

    initial

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -3,7 +3,7 @@
     <div class=" z-0 graditen h-full flex content-center flex-wrap">
 
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 mb-10">
-            <a href="#"
+            <a href="/product"
                 class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
                 role="alert">
                 <span class="text-sm font-medium bg-primary-600 rounded-full px-4 py-1.5 mr-3">Find out! What
@@ -19,7 +19,7 @@
             <p class="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">Explore our range of exquisite
                 yarns and silks, handpicked from the best sources across the globe.</p>
             <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="#"
+                <a href="/about"
                     class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Learn more
                     <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
