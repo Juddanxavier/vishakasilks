e219ad36c1f7fe1b57d4059d5dbd2fd52bee2dc7
Juddan <juddan2008@gmail.com>
2023-06-21 12:03:09 +0530

    initial

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
   <title>
     <?php wp_title(); ?> Vishaka Silks
   </title>
