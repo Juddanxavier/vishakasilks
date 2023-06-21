e9c437ffd9ba9f25b42ad3935c27965878eafc29
Juddan <juddan2008@gmail.com>
2023-06-22 00:08:44 +0530

    initial

--- single-product.php
+++ single-product.php
@@ -1,10 +1,16 @@
 <?php
 get_header(); ?>
 <?php
-while (have_posts()):
-    the_post();
-    get_template_part('template-parts/product-content', get_post_format());
-endwhile;
+do_action('woocommerce_before_main_content');
 ?>
-
+<?php
+while (have_posts()): ?>
+    <?php the_post(); ?>
+    // get_template_part('template-parts/product-content', get_post_format());
+    <?php wc_get_template_part('content', 'single-product'); ?>
+<?php endwhile;
+?>
+<?php
+do_action('woocommerce_after_main_content');
+?>
 <?php get_footer(); ?>
