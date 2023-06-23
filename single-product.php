dd72ea5b649d9fdd24162aa16a396a25524be4f3
Juddan <juddan2008@gmail.com>
2023-06-23 13:04:27 +0530

    initial

--- single-product.php
+++ single-product.php
@@ -65,3 +65,8 @@ get_header('shop'); ?>
     do_action('woocommerce_sidebar');
     ?>
 </div>
+
+<?php
+get_footer();
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
