7c48c2fb70f363ee5501ff017297f8447df84e93
Juddan <juddan2008@gmail.com>
2023-06-23 22:23:16 +0530

    initial

--- single-product.php
+++ single-product.php
@@ -60,12 +60,18 @@ get_header('shop'); ?>
     <div>
         <?php
         /**
-         * Hook: woocommerce_before_single_product_summary.
+         * Hook: Woocommerce_single_product_summary.
          *
-         * @hooked woocommerce_show_product_sale_flash - 10
-         * @hooked woocommerce_show_product_images - 20
+         * @hooked woocommerce_template_single_title - 5
+         * @hooked woocommerce_template_single_rating - 10
+         * @hooked woocommerce_template_single_price - 10
+         * @hooked woocommerce_template_single_excerpt - 20
+         * @hooked woocommerce_template_single_add_to_cart - 30
+         * @hooked woocommerce_template_single_meta - 40
+         * @hooked woocommerce_template_single_sharing - 50
+         * @hooked WC_Structured_Data::generate_product_data() - 60
          */
-        do_action('woocommerce_before_single_product_summary');
+        do_action('woocommerce_single_product_summary');
         ?>
     </div>
     <?php
