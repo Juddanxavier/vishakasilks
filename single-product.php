9f2bf4483ea370360a3d09bff78b9f4add082957
Juddan <juddan2008@gmail.com>
2023-06-23 22:17:05 +0530

    initial

--- single-product.php
+++ single-product.php
@@ -9,9 +9,9 @@
 </div>
 <?php
 /**
- * The Template for displaying all single products
+ * The template for displaying product content in the single-product.php template
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -20,54 +20,72 @@
  * the readme will list any important changes.
  *
  * @see         https://docs.woocommerce.com/document/template-structure/
- * @package     WooCommerce\Templates
- * @version     1.6.4
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
+ * @version     3.0.0
  */
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+    exit;
 }
 
-get_header('shop'); ?>
-<div class="mx-12 md:mx-24 my-12">
+/**
+ * Hook Woocommerce_before_single_product.
+ *
+ * @hooked wc_print_notices - 10
+ */
+do_action('woocommerce_before_single_product');
 
-    <!-- <?php
-    /**
-     * woocommerce_before_main_content hook.
-     *
-     * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-     * @hooked woocommerce_breadcrumb - 20
-     */
-    do_action('woocommerce_before_main_content');
-    ?> -->
-    <?php while (have_posts()): ?>
-        <?php the_post(); ?>
+if (post_password_required()) {
+    echo get_the_password_form(); // WPCS: XSS ok.
+    return;
+}
+?>
+<div id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="row">
+        <div class="col-5">
+            <?php
+            /**
+             * Hook: woocommerce_before_single_product_summary.
+             *
+             * @hooked woocommerce_show_product_sale_flash - 10
+             * @hooked woocommerce_show_product_images - 20
+             */
+            do_action('woocommerce_before_single_product_summary');
+            ?>
+            <div class="col-7">
+                <?php
+                /**
+                 * Hook: Woocommerce_single_product_summary.
+                 *
+                 * @hooked woocommerce_template_single_title - 5
+                 * @hooked woocommerce_template_single_rating - 10
+                 * @hooked woocommerce_template_single_price - 10
+                 * @hooked woocommerce_template_single_excerpt - 20
+                 * @hooked woocommerce_template_single_add_to_cart - 30
+                 * @hooked woocommerce_template_single_meta - 40
+                 * @hooked woocommerce_template_single_sharing - 50
+                 * @hooked WC_Structured_Data::generate_product_data() - 60
+                 */
+                do_action('woocommerce_single_product_summary');
+                ?>
+            </div>
+        </div>
 
-        <?php wc_get_template_part('content', 'single-product'); ?>
+        <?php
+        /**
+         * Hook: woocommerce_after_single_product_summary.
+         *
+         * @hooked woocommerce_output_product_data_tabs - 10
+         * @hooked woocommerce_upsell_display - 15
+         * @hooked woocommerce_output_related_products - 20
+         */
+        do_action('woocommerce_after_single_product_summary');
+        ?>
+    </div>
 
-    <?php endwhile; // end of the loop. ?>
-
-
+    <?php do_action('woocommerce_after_single_product'); ?>
     <?php
-    /**
-     * woocommerce_after_main_content hook.
-     *
-     * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-     */
-    do_action('woocommerce_after_main_content');
-    ?>
+    get_footer();
 
-    <?php
-    /**
-     * woocommerce_sidebar hook.
-     *
-     * @hooked woocommerce_get_sidebar - 10
-     */
-    do_action('woocommerce_sidebar');
-    ?>
-</div>
-
-<?php
-get_footer();
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+    /* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
