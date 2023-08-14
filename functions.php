67864384b464e6b98955b3c728c60101949037a6
Juddan <juddan2008@gmail.com>
2023-08-14 22:11:28 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ add_action('wp_enqueue_scripts', function () {
 });
 wp_enqueue_style('VISHAKA-tailwind', get_template_directory_uri() . '/assets/dist/main.bundle.css', array());
 
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 
 function register_my_menus()
 {
@@ -229,4 +228,7 @@ function add_woocommerce_support()
     add_theme_support('woocommerce');
 }
 add_action('after_setup_theme', 'add_woocommerce_support')
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+
     ?>
