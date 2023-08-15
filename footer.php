42b984601062f4d807dc58d90cb71a45d22a325f
Juddan <juddan2008@gmail.com>
2023-08-15 10:55:14 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="grid grid-row-auto text-white bg-[color:var(--theme-color-footer)] px-12 py-16">
         <div class="grid md:grid-cols-2 mb-14 pb-4 border-b-2 border-gray-400 border-opacity-25">
             <div class="flex">Vishaka Silks</div>
-            <div class="flex flex-row">
+            <div class="flex flex-row-reverse">
                 <?php wp_nav_menu(
                     array(
                         'menu_class' => 'menu-header flex gap-6 text-[#ffffff]',
